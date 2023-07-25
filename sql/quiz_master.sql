3976e5bcc1227b89599831d8f78d10cfe11dc95b
codywi <64983732+codywi@users.noreply.github.com>
2023-07-24 19:18:35 -0500

    Adjust query

--- sql/quiz_master.sql
+++ sql/quiz_master.sql
@@ -394,8 +394,7 @@ insert into `keywords` values
 CREATE TABLE `question_keywords` (
     `question_id` int(11) NOT NULL,
     `id` int(11) NOT NULL
-    foreign key (question_id) references questions(question_id),
-    foreign key (id) references keywords(keyword),
+
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
